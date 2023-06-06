11d6526427df2cab7712ed9594b4734ad2df5947
evaaldo <evaaldormj@gmail.com>
2023-06-06 15:25:41 -0300

    Feat: Extraindo lista de primeiros autores e autores correspondentes

--- extracao.py
+++ extracao.py
@@ -1,9 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+
+# #Inicializa o driver do Selenium
+# driver = webdriver.Chrome(executable_path=r'./chromedriver.exe')
+# driver.get("https://www.scielo.br/j/anp/a/z9dRGTnDrPgjWkS7cvgyLHw/?format=html&lang=en&stop=previous")
 
 #URL da página da Scielo
-URL = "https://www.scielo.br/j/anp/a/z9dRGTnDrPgjWkS7cvgyLHw/?format=html&lang=en&stop=previous"
+URL = "https://www.scielo.br/j/anp/a/wHRvqzKrVwtJY8KTW3bZRjS/?format=html&lang=en"
 
 #Acessando a página por meio da requisição HTTP
 response = requests.get(URL)
@@ -19,13 +25,19 @@ if(response.status_code == 200):
     fasciculos = []
     titulos = []
     citacoes = []
+    nomesPrimeiroAutor = []
+    nomesCorrespondenteAutor = []
 
-    #Informações buscadas
+    #Informações buscadas com BeatifulSoup
     ano = soup.find('span', class_='_editionMeta')
     fasciculo = soup.find('span', class_='_editionMeta')
     tituloIngles = soup.find('h1', class_='article-title')
     tituloPortugues = soup.find('h2', class_='article-title')
     citacaoLista = soup.find_all('ul', class_='refList')
+    autorPrincipalDiv = soup.find('div', class_='tutors')
+    autorPrincipal = autorPrincipalDiv.find('strong')
+    autorCorrespondenteDiv = soup.find('ul', class_='footnote')
+    autorCorrespondente = autorCorrespondenteDiv.find('li')
 
     #Isso tudo deve estar dentro de um loop
     #LOOP AQUI!!
@@ -35,6 +47,8 @@ if(response.status_code == 200):
     fasciculo_conteudo = fasciculo.text.strip()
     tituloIngles_conteudo = tituloIngles.text.strip()
     tituloPortugues_conteudo = tituloPortugues.text.strip()
+    autorPrincipal_conteudo = autorPrincipal.text.strip()
+    autorCorrespondente_conteudo = autorCorrespondente.text.strip()
 
     #loop necessário para capturar o número de informações, porém ainda faz parte da etapa de extração de textos
     #Checa cada <li> dentro da <ul>
@@ -59,15 +73,21 @@ if(response.status_code == 200):
 
     #Códigos para informações que precisam de padrão Regex
     padrao_fasciculo = re.compile(r'\((.*?)\)')
+    padrao_autorCorrespondente = r'Address for correspondence (.+?) \(email'
 
     #Valor esperado
     ano_conteudo = ano_conteudo.split("•")[-1].strip()
     fasciculo_conteudo = re.findall(padrao_fasciculo, fasciculo_conteudo)
     fasciculo_conteudo = fasciculo_conteudo[0].strip()
+    autorCorrespondente_conteudo = re.findall(padrao_autorCorrespondente, autorCorrespondente_conteudo)
+    autorCorrespondente_conteudo = autorCorrespondente_conteudo[0].strip()
+    autorCorrespondente_conteudo = autorCorrespondente_conteudo.replace(',', '')
     
     #Armazenamento
     anos.append(ano_conteudo)
     fasciculos.append(fasciculo_conteudo)
+    nomesPrimeiroAutor.append(autorPrincipal_conteudo)
+    nomesCorrespondenteAutor.append(autorCorrespondente_conteudo)
 
     #Condicional necessária para os títulos, porém ainda faz parte da etapa de armazenamento
     if(tituloPortugues_conteudo == ''):
@@ -81,3 +101,5 @@ if(response.status_code == 200):
     print("Lista de fascículos:", fasciculos)
     print("Lista de títulos:", titulos)
     print("Lista de citações:", citacoes)
+    print("Lista de nomes de primeiro autor:", nomesPrimeiroAutor)
+    print("Lista de nomes de autor correspondente:", nomesCorrespondenteAutor)
